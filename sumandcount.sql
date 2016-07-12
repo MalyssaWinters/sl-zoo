7ccffd38e32e80cb8099df4d5c268b7bb8ed156c
Malyssa Winters <malyssawinters5@gmail.com>
2016-07-12 19:37:56 -0400

    SUM and COUNT

---
+++ sumandcount.sql
@@ -0,0 +1,21 @@
+--SUM and COUNT
+
+--Show the total population of the world
+  SELECT SUM(population)
+  FROM world;
+
+--List all the continents - just once each
+  SELECT DISTINCT(continent)
+  FROM world;
+
+--Give the total GDP of Africa
+  SELECT SUM(gdp)
+  FROM world
+  WHERE continent = 'Africa';
+
+--How many countries have an area of at least 1000000
+  SELECT COUNT(name)
+  FROM world
+  WHERE area > 1000000;
+  
+--
